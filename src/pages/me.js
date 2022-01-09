ad2f4f0d858af612acf759e6fc1a1b9e52cff244
Ikenna Okpo <ikenna.okpo@aurea.com>
2022-01-09 02:44:22 +0000

    pages in gatsby

---
+++ src/pages/me.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Seo from "../components/seo"
+
+const Me = () => {
+    return (
+        <>
+            <Seo title="Me" description="This page talks about who I am."/>
+            <p>Hey, look at me! I'm super cool!</p>
+        </>
+    )
+}
+
+export default Me;
---
+++ src/pages/works.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Seo from "../components/seo"
+
+const Works = () => {
+  return (
+    <>
+      <Seo
+        title="Works"
+        description="This page talks about the works I have done."
+      />
+      <p>
+        Hey, I have also been pretty busy and have done some really cool
+        projects in the pase. Check them out!
+      </p>
+    </>
+  )
+}
+
+export default Works
